a481c542cb1a3a7cf70f74caeb6a01de9f469967
Anh Do <do.dinh.anh@linecorp.com>
2022-06-17 11:56:37 +0700

    chore: connecting to a remote peer

--- perfect-negotiation/public/main.js
+++ perfect-negotiation/public/main.js
@@ -1,3 +1,24 @@
+const constraints = { audio: true, video: true }
+const selfVideo = document.querySelector('video.selfview')
+const remoteVideo = document.querySelector('video.remoteview')
+
 const config = {
-  iceServers: []
-};
+  iceServers: [],
+}
+
+const signaler = new SignalingChannel()
+const pc = new RTCPeerConnection(config)
+
+async function start() {
+  try {
+    const stream = await navigator.mediaDevices.getUserMedia(constraints)
+
+    stream.getTracks().forEach(track => {
+      pc.addTrack(track, stream)
+    })
+
+    selfVideo.srcObject = stream
+  } catch (error) {
+    console.error(error)
+  }
+}
---
+++ perfect-negotiation/public/signaling-channel.js
@@ -0,0 +1,3 @@
+class SignalingChannel {
+  
+}
